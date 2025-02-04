9a9cbfeef787ee72399fcb6e90ed41a036f02b7e
Samyra Souza <samyragrilanda@gmail.com>
2025-02-03 22:46:16 -0300

    Começando frontend

---
+++ script/global/menu.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#pag").click(function () {
+        $("#click-nav_pag").slideToggle();
+    });
+    $("#adote").click(function () {
+        $("#click-nav_adote").slideToggle();
+    });
+});
